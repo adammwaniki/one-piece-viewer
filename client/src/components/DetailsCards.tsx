56de807e7bfec96c6bfdc834ee437899bc9d1edb
Adam Mwaniki <adamndegwa@protonmail.com>
2025-01-10 12:56:21 +0300

    Initial commit

---
+++ client/src/components/DetailsCards.tsx
@@ -0,0 +1,6 @@
+
+export default function DetailsCards() {
+  return (
+    <div>DetailsCards</div>
+  )
+}
---
+++ client/src/components/ImageCards.tsx
@@ -0,0 +1,6 @@
+
+export default function ImageCards() {
+  return (
+    <div>ImageCards</div>
+  )
+}
