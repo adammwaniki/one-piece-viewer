e508b6f95275a6d7fef194f66c6fcc1655a540c4
Adam Mwaniki <adamndegwa@protonmail.com>
2025-01-10 16:49:43 +0300

    Adds tailwind css

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,9 +1,21 @@
+import luffy_silhoutte from '../assets/images/luffy_silhoutte.svg'
 
 export default function Homepage() {
   return (
-    <div>
-      Homepage
+    <main className="max-h-svh w-full grid grid-rows-[auto_1fr] gap-3">{/**grid-rows-[auto_1fr] to create a dynamic two-row layout */}
+      <section className="flex justify-center">
+        <h1 className="text-[clamp(2rem,5vw,4rem)] font-bold"> {/*Text automatically scales using clamp() function: minimum 2rem, preferred 5vw, maximum 4rem */}
+          One Piece
+        </h1>
+      </section>
       
-    </div>
+      <section className="flex items-center justify-center">
+        <img
+          src={luffy_silhoutte}
+          alt="Luffy Silhouette"
+          className="w-auto h-[66.67svh]"
+        />
+      </section>
+    </main>
   )
 }
